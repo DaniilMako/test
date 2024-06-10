97bf87e447004f2256d3aafce2015ffdb3f8f4c4
unknown <d.makovetskii@g.nsu.ru>
2024-06-10 22:03:40 +0700

    tests

--- pytest.py
+++ pytest.py
@@ -1,25 +1,24 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch, MagicMock
+from tkinter import Tk
 from birdCLEF import AudioAnalysisApp
 
+
 class TestAudioAnalysisApp(unittest.TestCase):
-    def setUp(self):
-        self.app = AudioAnalysisApp()
-        self.app.choose_audio_file = MagicMock(return_value="XC400498.ogg")
+    @patch('birdCLEF.filedialog.askopenfilename', return_value='XC400498.ogg')
+    def test_choose_audio_file(self, mock_askopenfilename):
+        root = Tk()
+        app = AudioAnalysisApp()
+        app.choose_audio_file()
+        self.assertEqual(app.audio_file_path, 'XC400498.ogg')
+        self.assertTrue(mock_askopenfilename.called)
 
-    def test_choose_audio_file(self):
-        self.app.choose_audio_file()
-        self.app.choose_audio_file.assert_called_once()
+    def test_create_widgets(self):
+        root = Tk()
+        app = AudioAnalysisApp()
+        app.create_widgets()
+        self.assertTrue(app.result_tabs.winfo_exists())  # Assert that result_tabs exists
 
-    def test_load_model(self):
-        self.app.load_model = MagicMock(return_value="mocked_model")
-        model = self.app.load_model()
-        self.assertEqual(model, "mocked_model")
-
-    def test_mode_selection(self):
-        self.assertEqual(self.app.mode.get(), "С тишиной")
-        self.app.mode.set("Без тишины")
-        self.assertEqual(self.app.mode.get(), "Без тишины")
 
 if __name__ == '__main__':
     unittest.main()
