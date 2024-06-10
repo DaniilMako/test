0586180b523fac59c5eda8c53851fb000b581c21
unknown <d.makovetskii@g.nsu.ru>
2024-06-11 00:46:34 +0700

    tests

--- pytest.py
+++ pytest.py
@@ -1,19 +1,26 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+import tkinter as tk
+import matplotlib
+
+matplotlib.use('Agg')
+
 from birdCLEF import AudioAnalysisApp
 
 
 class TestAudioAnalysisApp(unittest.TestCase):
+    def setUp(self):
+        # Mock для tkinter
+        self.root = MagicMock()
+        tk.Tk = MagicMock(return_value=self.root)
+
     def test_create_widgets(self):
-        # root = Tk()
         app = AudioAnalysisApp()
         app.create_widgets()
-        # You can add more assertions here to check if widgets are created as expected
-        self.assertTrue(app.result_tabs.winfo_exists())  # Assert that result_tabs exists
+        self.assertTrue(app.result_tabs.winfo_exists())
 
     @patch('birdCLEF.filedialog.askopenfilename', return_value='XC400498.ogg')
     def test_choose_audio_file(self, mock_askopenfilename):
-        # root = Tk()
         app = AudioAnalysisApp()
         app.choose_audio_file()
         self.assertEqual(app.audio_file_path, 'XC400498.ogg')
