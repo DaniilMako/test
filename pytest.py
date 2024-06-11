f64efa73aabee6b11f2c7c106a3b5497c6efe84b
Alexander <103046655+Naxum3@users.noreply.github.com>
2024-06-11 13:42:19 +0700

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,11 +1,11 @@
+import tkinter as tk
 import unittest
 from unittest.mock import patch, MagicMock
-import tkinter as tk
+
 import matplotlib
+from main import AudioAnalysisApp
 
 matplotlib.use('Agg')
-
-from birdCLEF import AudioAnalysisApp
 
 
 class TestAudioAnalysisApp(unittest.TestCase):
@@ -18,13 +18,38 @@ class TestAudioAnalysisApp(unittest.TestCase):
         app = AudioAnalysisApp()
         app.create_widgets()
         self.assertTrue(app.result_tabs.winfo_exists())
+        self.assertTrue(app.result_tabs.winfo_children())
 
-    @patch('birdCLEF.filedialog.askopenfilename', return_value='XC400498.ogg')
+    @patch('main.filedialog.askopenfilename', return_value='XC400498.ogg')
     def test_choose_audio_file(self, mock_askopenfilename):
         app = AudioAnalysisApp()
         app.choose_audio_file()
         self.assertEqual(app.audio_file_path, 'XC400498.ogg')
         self.assertTrue(mock_askopenfilename.called)
+
+    @patch('main.filedialog.askopenfilename', return_value='XC400498.ogg')
+    def test_display_spectrogram(self, mock_spectrogram_tab):
+        app = AudioAnalysisApp()
+        app.start_analysis()
+        self.assertTrue(mock_spectrogram_tab.winfo_children())
+
+    @patch('main.filedialog.askopenfilename', return_value='XC400498.ogg')
+    def test_display_graphs(self, mock_graphs_tab):
+        app = AudioAnalysisApp()
+        app.start_analysis()
+        self.assertTrue(mock_graphs_tab.winfo_children())
+
+    @patch('main.filedialog.askopenfilename', return_value='XC400498.ogg')
+    def test_display_metrics(self, mock_metrix_tab):
+        app = AudioAnalysisApp()
+        app.start_analysis()
+        self.assertTrue(mock_metrix_tab.winfo_children())
+
+    @patch('main.filedialog.askopenfilename', return_value='XC400498.ogg')
+    def test_display_predictions(self, mock_predictions_tab):
+        app = AudioAnalysisApp()
+        app.start_analysis()
+        self.assertTrue(mock_predictions_tab.winfo_children())
 
 
 if __name__ == '__main__':
